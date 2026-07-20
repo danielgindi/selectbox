e71af714f67a56ba69ef216b1daf9dea66bbf1df
Daniel Cohen Gindi <danielgindi@gmail.com>
2026-07-20 10:13:16 +0300

    chore(build): migrated to vite

--- scripts/build.js
+++ scripts/build.js
@@ -1,26 +1,71 @@
 /* eslint-disable no-console */
 
-const FsExtra = require('fs-extra');
 const Fs = require('fs');
 const Path = require('path');
 
+const MagicString = require('magic-string');
+const Sass = require('sass');
+
 (async () => {
 
-    await FsExtra.emptyDir('./dist');
-    await FsExtra.emptyDir('./css');
+    const { build } = await import('vite');
 
-    const Rollup = require('rollup');
+    await Fs.promises.rm('./dist', { recursive: true, force: true });
+    await Fs.promises.rm('./css', { recursive: true, force: true });
+    await Fs.promises.mkdir('./dist', { recursive: true });
+    await Fs.promises.mkdir('./css', { recursive: true });
 
-    const rollupTasks = [{
+    const pkg = require('../package.json');
+    const banner = [
+        `/*!`,
+        ` * ${pkg.name} ${pkg.version}`,
+        ` * ${pkg.repository.url}`,
+        ' */\n',
+    ].join('\n');
+
+    const globals = {
+        '@danielgindi/dom-utils/lib/Dom': 'domUtilsDom',
+        '@danielgindi/dom-utils/lib/DomCompat': 'domUtilsDomCompat',
+        '@danielgindi/dom-utils/lib/Css': 'domUtilsCss',
+        '@danielgindi/dom-utils/lib/DomEventsSink': 'domUtilsDomEventsSink',
+        '@danielgindi/virtual-list-helper': 'VirtualListHelper',
+        'keycode-js': 'keyCode',
+        'mitt': 'mitt',
+    };
+
+    const external = (id) => {
+        return id === 'keycode-js' ||
+            id === 'mitt' ||
+            id.startsWith('@danielgindi/dom-utils') ||
+            id.startsWith('@danielgindi/virtual-list-helper');
+    };
+
+    const bannerPlugin = {
+        name: 'selectbox-banner',
+
+        renderChunk(code) {
+            const magicString = new MagicString(code);
+            magicString.prepend(banner);
+
+            return {
+                code: magicString.toString(),
+                map: magicString.generateMap({
+                    hires: true,
+                }),
+            };
+        },
+    };
+
+    const buildTasks = [{
         dest: 'dist/lib.es6.js',
         sourceMap: true,
-        outputFormat: 'esm',
+        outputFormat: 'es',
         minified: false,
         ecmaVersion: 2022,
     }, {
         dest: 'dist/lib.es6.min.js',
         sourceMap: true,
-        outputFormat: 'esm',
+        outputFormat: 'es',
         minified: true,
         ecmaVersion: 2022,
     }, {
@@ -28,7 +73,6 @@ const Path = require('path');
         sourceMap: true,
         outputFormat: 'umd',
         outputName: 'SelectBox',
-        babelTargets: '> 0.25%, not dead',
         minified: false,
         ecmaVersion: 2022,
     }, {
@@ -36,7 +80,6 @@ const Path = require('path');
         sourceMap: true,
         outputFormat: 'umd',
         outputName: 'SelectBox',
-        babelTargets: '> 0.25%, not dead',
         minified: true,
         ecmaVersion: 2022,
     }, {
@@ -44,7 +87,6 @@ const Path = require('path');
         sourceMap: true,
         outputFormat: 'cjs',
         outputName: 'SelectBox',
-        babelTargets: '> 0.25%, not dead',
         minified: false,
         ecmaVersion: 2022,
     }, {
@@ -52,125 +94,57 @@ const Path = require('path');
         sourceMap: true,
         outputFormat: 'cjs',
         outputName: 'SelectBox',
-        babelTargets: '> 0.25%, not dead',
         minified: true,
         ecmaVersion: 2022,
     }];
 
-    const inputFile = 'lib/index.js';
-
-    for (let task of rollupTasks) {
+    for (let task of buildTasks) {
         console.info('Generating ' + task.dest + '...');
 
-        let plugins = [
-            require('@rollup/plugin-node-resolve').nodeResolve({
-                mainFields: ['module', 'main'],
-            }),
-            require('@rollup/plugin-commonjs')({}),
-        ];
+        const plugins = [bannerPlugin];
 
-        const pkg = require('../package.json');
-        const banner = [
-            `/*!`,
-            ` * ${pkg.name} ${pkg.version}`,
-            ` * ${pkg.repository.url}`,
-            ' */\n',
-        ].join('\n');
 
-        if (task.babelTargets) {
-            plugins.push(require('@rollup/plugin-babel').babel({
-                sourceMap: !!task.sourceMap,
-                presets: [
-                    ['@babel/env', {
-                        targets: task.babelTargets,
-                        useBuiltIns: 'usage',
-                        corejs: 3,
-                    }],
-                ],
-                compact: false,
-                minified: false,
-                comments: true,
-                retainLines: true,
-                babelHelpers: 'bundled',
-                exclude: 'node_modules/**/core-js/**/*',
-            }));
-        }
-
-        if (task.minified) {
-            plugins.push(require('@rollup/plugin-terser')({
-                toplevel: true,
-                compress: {
-                    ecma: task.ecmaVersion,
-                    passes: 2,
-                },
-                sourceMap: !!task.sourceMap,
-            }));
-        }
-
-        plugins.push({
-            name: 'banner',
-
-            renderChunk(code, chunk, _outputOptions = {}) {
-
-                const magicString = new (require('magic-string'))(code);
-                magicString.prepend(banner);
-
-                return {
-                    code: magicString.toString(),
-                    map: magicString.generateMap({
-                        hires: true,
-                    }),
-                };
-            },
-        });
-
-        const bundle = await Rollup.rollup({
-            preserveSymlinks: true,
-            treeshake: false,
-            onwarn(warning, warn) {
-                if (warning.code === 'THIS_IS_UNDEFINED') return;
-                warn(warning);
-            },
-            input: inputFile,
+        await build({
+            configFile: false,
+            publicDir: false,
+            root: process.cwd(),
             plugins: plugins,
-            external: [
-                /^@danielgindi\/dom-utils(\/|$)/,
-                /^@danielgindi\/virtual-list-helper(\/|$)/,
-                'keycode-js',
-                'mitt',
-            ],
-        });
-
-        let generated = await bundle.generate({
-            name: task.outputName,
-            sourcemap: task.sourceMap,
-            format: task.outputFormat,
-            exports: task.outputExports,
-            globals: {
-                '@danielgindi/dom-utils/lib/Dom': 'domUtilsDom',
-                '@danielgindi/dom-utils/lib/DomCompat': 'domUtilsDomCompat',
-                '@danielgindi/dom-utils/lib/Css': 'domUtilsCss',
-                '@danielgindi/dom-utils/lib/DomEventsSink': 'domUtilsDomEventsSink',
-                '@danielgindi/virtual-list-helper': 'VirtualListHelper',
-                'keycode-js': 'keyCode',
-                'mitt': 'mitt',
+            resolve: {
+                mainFields: ['module', 'main'],
+                preserveSymlinks: true,
+            },
+            build: {
+                emptyOutDir: false,
+                lib: {
+                    entry: Path.resolve(process.cwd(), 'lib/index.js'),
+                    name: task.outputName,
+                    formats: [task.outputFormat],
+                    fileName: () => Path.basename(task.dest),
+                },
+                minify: task.minified ? 'terser' : false,
+                outDir: Path.dirname(task.dest),
+                sourcemap: task.sourceMap,
+                target: 'es2022',
+                terserOptions: {
+                    toplevel: true,
+                    compress: {
+                        ecma: task.ecmaVersion,
+                        passes: 2,
+                    },
+                },
+                rolldownOptions: {
+                    external: external,
+                    treeshake: false,
+                    output: {
+                        globals: globals,
+                        codeSplitting: false,
+                    },
+                },
             },
         });
-
-        let code = generated.output[0].code;
-
-        if (task.sourceMap === true && generated.output[0].map) {
-            let sourceMapOutPath = task.dest + '.map';
-            FsExtra.writeFileSync(sourceMapOutPath, generated.output[0].map.toString());
-            code += '\n//# sourceMappingURL=' + Path.basename(sourceMapOutPath);
-        }
-
-        FsExtra.writeFileSync(task.dest, code);
     }
 
     console.info('Generating css files....');
-
-    const Sass = require('sass');
 
     for (let item of [
         { src: './scss/droplist.scss', dest: './css/droplist.css' },
@@ -182,7 +156,8 @@ const Path = require('path');
         });
         await Fs.promises.writeFile(item.dest, compiledSassData.css + `\n\n/*# sourceMappingURL=${Path.basename(item.dest)}.map */`);
         await Fs.promises.writeFile(item.dest + '.map', JSON.stringify(compiledSassData.sourceMap)
-            .replace(/"sources":\["([^"]+)"]/, (m, x) => '"sources":["../scss/' + x.substr(x.lastIndexOf('/') + 1) + '"]'));
+            .replace(/"sources":\["([^"]+)"]/,
+                (_m, x) => '"sources":["../scss/' + x.substr(x.lastIndexOf('/') + 1) + '"]'));
     }
 
     console.info('Done.');
